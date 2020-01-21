248be26109bb52a4935f1f2fe93b827d1be2e49e
Thapa <ujjwalthapa2307@gmail.com>
2020-01-21 16:24:46 +0900

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
+  config.force_ssl = true
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
