031abdc51052c66151b5d33776ccc398da54b5df
Thapa <ujjwalthapa2307@gmail.com>
2020-01-27 14:18:24 +0900

    Pagination of Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users  = User.paginate(:page => params[:page], :per_page=>5)
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -38,8 +44,16 @@ class UsersController < ApplicationController
     # confirms a logged-in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
