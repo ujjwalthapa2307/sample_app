367d3b25c61b109bfebb8e17e3c631ebc6241abc
Thapa <ujjwalthapa2307@gmail.com>
2020-01-29 15:56:55 +0900

    changed heroku address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
 
-  host = '<your heroku app>.herokuapp.com'
+  host = 'frozen-spire-25429.herokuapp.com'
 
   config.action_mailer.default_url_options = { host: host }
 
