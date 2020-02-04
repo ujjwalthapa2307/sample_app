142044666b80df24a837e70c4c54980ab8b0fe32
Thapa <ujjwalthapa2307@gmail.com>
2020-02-04 18:04:28 +0900

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,24 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
 end
