c03f6e4edde4454b64331a4d8197e7f8430d6cc8
Thapa <ujjwalthapa2307@gmail.com>
2020-02-04 14:54:33 +0900

    Creating microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -25,4 +25,9 @@ class MicropostTest < ActiveSupport::TestCase
       @micropost.content = "a" * 141
       assert_not @micropost.valid?
     end
+
+    test "order should be most recent first" do
+      assert_equal Micropost.first, microposts(:most_recent)
+    end
+
 end
