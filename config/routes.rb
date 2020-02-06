a7419aa50ddb2c391784eb5d5f5981e0f322731b
Thapa <ujjwalthapa2307@gmail.com>
2020-02-06 11:18:23 +0900

    Following and Followers Pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
   get   '/login',   to: 'sessions#new'
   post  '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+   resources :users do
+     member do
+       get :following, :followers
+     end
+   end
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
